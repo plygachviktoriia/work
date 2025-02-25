d74fe106cb84d64a32677187e2411c45ef8cd126
quer <xplyhach@stuba.sk>
2025-02-25 20:49:45 +0200

    first commit

---
+++ viktoriia mpm.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+ int main(){
+     int i, mnozina1 = {3,5,2,6}, mnozina2 = {8,2,9,1,5};
+     // scanf("%d", &mnozina1);
+     //  scanf("%d", &mnozina2);
+     int zjednotenia = mnozina1 + mnozina2;
+     for (i = 1; i<=zjednotenia; i++){
+          printf("%d", zjednotenia);
+     }
+     
+      printf("Zjednotenia mnoziny1 a mnoziny2:");
+     // scanf("%d", &zjednotenia);
+      
+     
+        
+    return 0;  
