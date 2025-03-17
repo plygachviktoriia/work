292c3bde4b148b70c09fe9d33ff340ab5e2ca9bd
Plygach Viktoriia <xplyhach@stuba.sk>
2025-03-17 22:28:33 +0100

    Update be,le.c

--- be,le.c
+++ be,le.c
@@ -4,7 +4,7 @@ int main() {
      
      short n = 0x1234;
      char low_byte = n % 0x100;
-     char left_byte = *(char*)&n;
+     char left_byte = *(short*)&n;
      
      if (low_byte == left_byte)
         printf("little endian");
